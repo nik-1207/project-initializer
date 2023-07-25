9b0ea060339841a9559736c854891ddad78b3cbe
nik-1207 <nmknikhilkashyap@gmail.com>
2023-07-25 21:46:36 +0530

    npm init

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,12 +1,15 @@
 import { mkdir } from "fs/promises";
 import { join } from "path";
 import { ProjectInfo } from "../types";
+import { spawn } from "child_process";
 
 export async function createDir(path: string): Promise<void> {
   await mkdir(path);
 }
 
-export async function initialize({ projectTitle }: ProjectInfo): Promise<void> {
+export async function initialize({ projectTitle, packageManager }: ProjectInfo): Promise<void> {
   const path = join(process.cwd(), projectTitle);
-  createDir(path);
+  await createDir(path);
+  console.log(packageManager);
+  spawn(packageManager, ["init"]);
 }
