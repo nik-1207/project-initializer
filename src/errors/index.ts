0569a92eef409d6c5b108720202aa36b87d8bace
nik-1207 <nmknikhilkashyap@gmail.com>
2023-07-25 21:16:18 +0530

    minor

---
+++ src/errors/index.ts
@@ -0,0 +1,8 @@
+export class UnsupportedError extends Error {
+  message: string;
+  code = "ERR_UNSUPPORTED";
+  name = "Unsupported Error";
+  constructor(message: string) {
+    super(message);
+  }
+}
